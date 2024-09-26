948d0f24782ba980d3c3060f1eea9ffcb37c59bc
weitang <liy20084@gmail.com>
2024-09-26 13:12:40 +0900

    Add test stage and lambda stack

--- lib/cicd-codedepoly-stack.ts
+++ lib/cicd-codedepoly-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CicdCodedepolyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'popeline', {
+    const pipeline = new CodePipeline(this, 'popeline', {
       pipelineName: 'pipeline',
       synth: new ShellStep('synth', {
         input: CodePipelineSource.gitHub('weitanglo/cicd-codedeploy', 'main'),
@@ -17,5 +18,9 @@ export class CicdCodedepolyStack extends cdk.Stack {
         primaryOutputDirectory: 'cdk.out'
       })
     })
+
+    const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage',{
+      stageName: 'test'
+    }));
   }
 }
